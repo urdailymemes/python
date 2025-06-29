90fd4d3d9a16ad84cead01a1bfe87ebf7b30382e
urdailymemes <mira.isabel.pereira@gmail.com>
2025-06-29 15:33:58 +0530

    Update sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -1,1 +1,24 @@
+from math import sqrt
+
+def my_sqrt(num):
+
+  est = num / 2
+  for i in range(1, 6):
+    est = (est + num / est) / 2
+
+  return est
+
+
+est = my_sqrt(100)
+print("est sqrt =", est)
+print("math sqrt =", sqrt(100))
+
+est = my_sqrt(40)
+print("est sqrt =", est)
+print("math sqrt =", sqrt(40))
+
+est = my_sqrt(72)
+print("est sqrt =", est)
+print("math sqrt =", sqrt(72))
+
 
