539d21b4cadc178f83f142c8e6767c9f8c967893
urdailymemes <mira.isabel.pereira@gmail.com>
2025-08-07 11:44:09 +0000

    new tes

---
+++ test.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry", "grape"]
+for fruit in fruits:
+  print(fruit)
+
+fruits_1 = ["apple", "banana"]
+fruits_1.append("orange")
+print(fruits_1)
+print(len(fruits_1))
+
+animals = ["dog", "cat", "hamster", "rabbit"]
+print(len(animals))
